157722e35b97b4282464f53b5b6ecd0951c82f2b
abvl <vladislav.abrosimov@ananas.rs>
2023-04-06 13:12:50 +0200

    init

---
+++ src/modules/quiz/quiz.controller.ts
@@ -0,0 +1,15 @@
+import {Controller, Get} from '@nestjs/common';
+import {QuizService} from "./quiz.service";
+
+@Controller('quiz')
+export class QuizController {
+
+  constructor(private readonly quizService: QuizService) {
+  }
+
+  @Get()
+  getQuiz() {
+    return this.quizService.getQuiz();
+  }
+
+}
